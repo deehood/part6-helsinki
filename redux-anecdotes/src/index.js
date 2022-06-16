4fdc06c2a18b8ff223f5db1666a8af3b9206941c
deehood <dee.hood@gmail.com>
2022-06-16 19:53:02 +0100

    transfer

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { createStore } from 'redux'
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(reducer)
+import { Provider } from "react-redux";
+import App from "./App";
+import reducer from "./reducers/anecdoteReducer";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// const store = createStore(reducer);
+
+const store = configureStore({
+  reducer: {
+    blogs: blo,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
